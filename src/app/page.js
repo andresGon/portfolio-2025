f363e0f4af6897dbd7477d6e13bf077a13bf9465
andres gonzalez <andres.gonzalez@cluvi.com>
2024-10-03 16:59:05 -0500

    avance hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,21 @@
 import styles from "./globals.scss";
 
 export default function Home() {
-  return (
+  
+   return (
     <div className="mainHero">
       <div className="folio">Folio 2024</div>
       <div className="lang">Esp</div>
       <div className="inner">
         <div className="name">
-          <span>Andrés</span>
+          <span className="main-name">Andrés</span>
           <span> González</span> 
         </div>
         <div className="charge">Front end / ui developer</div>
         <nav>
           <button>Work</button>
-          <button>Expertise</button>
+          <button>Skills</button>
+          <button>About me</button>
           <button>Contact</button>
         </nav>
       </div>
